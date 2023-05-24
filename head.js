a8fd8ccb20ea62eba616623f871a0e3cfcb31239
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-05-23 19:20:07 -0700

    Create a function tail which returns the "tail" of an array: everything except for the first item (head) of the provided array.

--- head.js
+++ head.js
@@ -5,13 +5,12 @@ const assertEqual = function(actual, expected) {
     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
- function head (arr)
- {
-    if (arr.length === 0)
-      return undefined;
-    else
-      return arr[0];
- }
+const head = function(arr) {
+  // if (arr.length === 0)
+  //   return undefined;
+  // else
+  return arr[0];
+};
 
 // assertEqual(head([5,6,7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
