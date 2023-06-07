463384d82a4291f89b1019f9d6d6ff3bdbc662fd
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-06 23:07:14 -0700

    Implement a function which will take in two arrays, compare and console.log an appropriate message.

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+const eqArrays = (firstArray, secondArray) => {
+  if (firstArray.length !== secondArray.length)
+    return (false);
+  
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return (false);
+    }
+  }
+
+  return (true);
+}
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray)) {
+    console.log (`✅ Assertion Passed: ${firstArray} === ${secondArray}`);
+  }
+  else {
+    console.log  (`❌ Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+// assertArraysEqual([1, 2, 3], [1, 2]); // => false
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
--- eqArrays.js
+++ eqArrays.js
@@ -18,7 +18,7 @@ const eqArrays = (firstArray, secondArray) => {
   return (true);
 }
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => true
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)); // => false
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)); // => true
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)); // => false
+// console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => true
+// console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)); // => false
+// console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)); // => true
+// console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)); // => false
