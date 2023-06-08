4998078edb5aaff7611e3cf1a5ef760b2d386729
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-08 14:59:31 -0700

    Create a function that returns all the indices (0 based positions) in the string for each character.

---
+++ letterPositions.js
@@ -0,0 +1,42 @@
+const eqArrays = (firstArray, secondArray) => {
+  if (firstArray.length !== secondArray.length)
+    return (false);
+  
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return (false);
+    }
+  }
+
+  return (true);
+}
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray)) {
+    console.log (`✅ Assertion Passed: ${firstArray} === ${secondArray}`);
+  }
+  else {
+    console.log  (`❌ Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  //sentence = sentence.replace(/( )/g, "");
+  
+  for (let i = 0; i < sentence.length; i++) {
+    if (!results[sentence[i]]) {
+      results[sentence[i]] = [];
+    } 
+    
+    results[sentence[i]].push(i);
+  }
+
+  delete results[" "];
+
+  return results;
+};
+
+
+console.log(letterPositions("lighthouse in the house"));
+//assertArraysEqual(letterPositions("hello").e, [1]);
