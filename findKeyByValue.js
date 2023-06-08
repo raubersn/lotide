a4d3ebbdc900d1e21a0e9293c9f9550ad309d266
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-08 16:36:47 -0700

    Create a the function which scan a object and return the first key which contains the given value.

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    return (`✅ Assertion Passed: ${actual} === ${expected}`);
+  else
+    return  (`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+
+const findKeyByValue = (myObject, targetValue) => {
+  let targetKey;
+  const allValues = Object.values(myObject);
+
+  for (let i = 0; i < allValues.length; i++) {
+    if (allValues[i] === targetValue)
+    {
+      targetKey = Object.keys(myObject)[i];
+      break;
+    }
+  }
+
+  return (targetKey);
+}
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
--- letterPositions.js
+++ letterPositions.js
@@ -38,5 +38,5 @@ const letterPositions = function(sentence) {
 };
 
 
-console.log(letterPositions("lighthouse in the house"));
+//console.log(letterPositions("lighthouse in the house"));
 //assertArraysEqual(letterPositions("hello").e, [1]);
