c1785cd50af559bee80c223a0043df6d1b096377
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-08 14:21:55 -0700

    Create a  function that takes a collection of items and return counts for a specific subset of items

---
+++ countOnly.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    return (`✅ Assertion Passed: ${actual} === ${expected}`);
+  else
+    return  (`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countOnly = (allItems, itemsToCount) => {
+  const results = {}
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+/*
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+console.log(assertEqual(result1["Jason"], 1));
+console.log(assertEqual(result1["Karima"], undefined));
+console.log(assertEqual(result1["Fang"], 2));
+console.log(assertEqual(result1["Agouhanna"], undefined));
+*/
