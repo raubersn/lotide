963b271123109b898ebc6fd9b1cf7043a369c456
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-09 00:30:54 -0700

    Use recursion to allow eqArrays to support sub-arrays.

--- eqArrays.js
+++ eqArrays.js
@@ -3,12 +3,16 @@ const eqArrays = (firstArray, secondArray) => {
     return (false);
   
   for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
+    if (Array.isArray(firstArray[i])) {
+      if (!eqArrays(firstArray[i], secondArray[i])) {
+        return (false);
+      }
+    } else if (firstArray[i] !== secondArray[i]) {
       return (false);
     }
   }
 
   return (true);
-}
+};
 
 module.exports = eqArrays;
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -5,3 +5,8 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
