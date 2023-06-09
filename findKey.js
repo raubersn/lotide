4a29782bb7670709550e23a2dc82c72be6497765
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-08 22:49:45 -0700

    Create a function which scan a object and return the first key for which the callback returns true.

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    return (`✅ Assertion Passed: ${actual} === ${expected}`);
+  else
+    return  (`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+
+const findKey = (object, callback) => {
+  let targetKey;
+  const allKeys = Object.keys(object);
+
+  for (let i = 0; i < allKeys.length; i++) {
+    if (callback(object[allKeys[i]]))
+    {
+      return (allKeys[i]);
+    }
+  }
+}
+
+/*
+console.log(assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"));
+*/
