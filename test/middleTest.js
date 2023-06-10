e3437a4bc1a58af6d08bce5752be5f02b4bc96f5
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-09 17:49:57 -0700

    Refactor tailTest.js and middleTest.js to use Mocha & Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,17 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const middle = require("../middle");
 
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle", () => {
+  it("For arrays with one or two elements, there is no middle. Return an empty array", () => {
+    assert.deepEqual(middle([1]), []);
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("For arrays with odd number of elements, an array containing a single middle element should be returned.", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("For arrays with an even number of elements, an array containing the two elements in the middle should be returned", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,17 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const tail = require("../tail");
 
 // Test Case: Check the original array
-let words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words));
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-words = [1];
-console.log(tail(words));
-assertEqual(tail(words).length, 0); // An array with only one element should yield an empty array for its tail
-
-words = [];
-console.log(tail(words));
-assertEqual(tail(words).length, 0); // An empty array should yield an empty array for its tail
+describe ("#tail", () => {
+  it("returns [\"Lighthouse\", \"Labs\"] for [\"Yo Yo\", \"Lighthouse\", \"Labs\"] keeping the 3 elements in the original array", () => {
+    let words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words), ["Lighthouse", "Labs"]);
+    assert.strictEqual(words.length, 3); // original array should still have 3 elements!
+  });
+  it("An array with only one element should yield an empty array for its tail", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it("An empty array should yield an empty array for its tail", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
